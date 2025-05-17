dee0f7c4a5154f792279d731c002121df672ec8c
stanescustefan <stanescustefan58@gmail.com>
2025-05-17 15:55:37 +0300

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.static',
+    'django.contrib.staticfiles',
     'store.apps.StoreConfig',
     'cart.apps.CartConfig',
     'search.apps.SearchConfig',
