5140acf8af5a04232083656c416d791184866ad9
stanescustefan <stanescustefan58@gmail.com>
2025-05-17 16:30:39 +0300

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 MEDIA_URL = '/media/'
 
